5ec10248a7642cc5670fc3ae36f762a7f93d1a2e
EE02323 <Enas.elsayed@uranium-corp.com>
2024-01-01 12:25:17 +0200

    end

--- componts/PromptCard.jsx
+++ componts/PromptCard.jsx
@@ -12,9 +12,10 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
   const [copied, setCopied] = useState("");
 
   const handleProfileClick = () => {
-    console.log(post.creator._id === session?.user.id, "important");
-    // if (post.creator._id === session?.user.id) return router.push("/profile");
-    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+    if (post.creator._id === session?.user.id)
+      return router.push(
+        `/profile/${post.creator._id}?name=${post.creator.username}`
+      );
   };
 
   const handleCopy = () => {
